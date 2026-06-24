c85008cbc24b8bc954952daa91ebae0fcf71acef
deklinfisler <Deklinfis@outlook.com>
2026-06-23 20:58:08 -0700

    stuff

--- Assets/scripts/hitbox.cs
+++ Assets/scripts/hitbox.cs
@@ -27,7 +27,7 @@ public class hitbox : MonoBehaviour
             health.currentHealth -= GetComponentInParent<damagemangeer>().pycomain;
             GetComponentInParent<damagemangeer>().StopCoroutine(GetComponentInParent<damagemangeer>().hitboxCoroutine);
         }
-        GetComponent<AudioSource>().Play(); 
+        FindFirstObjectByType<hitSound>().Play(); 
     }
    void Update ()
     {
