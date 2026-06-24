c126c0204f67778d9ba6810cb7e97a00c98cbc23
deklinfisler <Deklinfis@outlook.com>
2026-06-23 22:27:44 -0700

    crush them

--- Assets/scripts/crushthem.cs
+++ Assets/scripts/crushthem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class crushthem : MonoBehaviour
 {
+    public bool crushing;
     public float crushfrequency = 10f;
     public float crushstrength = 10f;
     private float originalY;
@@ -17,21 +18,38 @@ public class crushthem : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse2) && GetComponent<PlayerMovement>().isGrounded == true)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
-            GetComponent<damagemangeer>().spawnhitbox(1.1f);
-            StartCoroutine(crush());
+             StartCoroutine(crushhitbox());
+             if(crushing == true)
+            {
+             transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+             GetComponent<damagemangeer>().spawnhitbox(1.1f);
+             StartCoroutine(crush());  
+             GetComponent<damagemangeer>().spawnhitbox(1.1f);
+             float x = transform.position.x;
+             float y = Mathf.Sin(Time.time * crushfrequency) * crushstrength + originalY;
+             float z = transform.position.z;
 
+             transform.position = new Vector3(x, y, z);
+             Destroy(gameObject, 2f);  
+             crushing = false;
+            }
+        
         }
     }
     IEnumerator crush()
     {
         yield return new WaitForSeconds(3);
-        GetComponent<damagemangeer>().spawnhitbox(1.1f);
-        float x = transform.position.x;
-            float y = Mathf.Sin(Time.time * crushfrequency) * crushstrength + originalY;
-            float z = transform.position.z;
+        crushing = true;
+                //Vector3 rotation = Camera.main.transform.rotation.eulerAngles;
+        //rotation.x = 0;
+       // rotation.z = 0;
 
-            transform.position = new Vector3(x, y, z);
-        Destroy(gameObject, 2f);
+        //GameObject crushthemhitbox = Instantiate(GetComponent<damagemangeer>().hitbox, transform.position + Camera.main.transform.forward * 2, Quaternion.identity);
+        //crushthemhitbox.transform.SetParent(transform);
+    }
+    IEnumerator crushhitbox()
+    {
+        yield return new WaitForSeconds(3);
+        GetComponent<damagemangeer>().spawnhitbox(1.1f);
     }
 }
