85459c80a193eeba0e4cbed5370f5cf9623f0f06
deklinfisler <Deklinfis@outlook.com>
2026-08-21 23:26:32 -0700

    e
    e

--- Assets/scripts/crushthem.cs
+++ Assets/scripts/crushthem.cs
@@ -8,6 +8,7 @@ public class crushthem : MonoBehaviour
     public bool crushing;
     public float crushfrequency = 10f;
     public float crushstrength = 10f;
+    public PlayerMovement playerMovement;
     private float originalY;
      void Awake()
     {
@@ -17,10 +18,15 @@ public class crushthem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse2) && GetComponent<PlayerMovement>().isGrounded == true)
+          if (other.TryGetComponent(out testdumby health))
+        {
+            health.currentHealth -= GetComponentInParent<damagemangeer>().pycocrush;
+            GetComponentInParent<damagemangeer>().StopCoroutine(GetComponentInParent<damagemangeer>().hitboxCoroutine);
+        }
+        if (Input.GetMouseButtonDown(1) && playerMovement.isGrounded == true)
         {
             StartCoroutine(crushhitbox());
-            
+            Debug.Log("crush ability activated");
             
             
                 //StartCoroutine(thecrushing());
@@ -61,12 +67,12 @@ public class crushthem : MonoBehaviour
          GetComponent<PlayerMovement>().movmentenabled = false;
         GetComponent<PlayerMovement>().canjump = false;
         yield return new WaitForSeconds(3);
-        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0, transform.position.z);
         Vector3 rotation = Camera.main.transform.rotation.eulerAngles;
         rotation.x = transform.position.x;
         rotation.y = Mathf.Sin(Time.time * crushfrequency) * crushstrength + originalY;
         rotation.z = transform.position.z;
-
+           
         GameObject crushhitbox = Instantiate(GetComponent<damagemangeer>().crushhitbox, transform.position + Camera.main.transform.forward * 2, Quaternion.identity);
         crushhitbox.transform.SetParent(transform);
          
@@ -75,6 +81,6 @@ public class crushthem : MonoBehaviour
           yield return new WaitForSeconds(1.5f);
         GetComponent<PlayerMovement>().movmentenabled = true;
         GetComponent<PlayerMovement>().canjump = true;
-       
+       GetComponent<damagemangeer>().attacking = true;
     }
 }
